4d407f04d98fecc7f89e99cb28675773dcc39e52
tbkoo53 <wokpoo@yahoo.co.jp>
2016-09-07 12:41:49 +0000

    提出２

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
     validates :name, length: { maximum: 20 }, presence: true
     validates :body, length: { minimum: 2, maximum: 30 }, presence: true
-    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 1 } #１以上
+    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 } #１以上
                        
 end
