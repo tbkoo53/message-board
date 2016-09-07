ea455c111312e45ec941ff13e7db844b770f7cdd
tbkoo53 <wokpoo@yahoo.co.jp>
2016-09-07 09:23:38 +0000

    削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     @message = Message.new
@@ -27,6 +27,12 @@ class MessagesController < ApplicationController
       render 'edit'
     end  
   end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end  
+    
   
   private
   def message_params
